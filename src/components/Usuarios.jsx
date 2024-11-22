b9d3ee3d7b9b529f16a043643539cb5188522494
javiers78889 <javiers78889@gmail.com>
2024-11-22 02:49:09 -0500

    listo

--- src/components/Usuarios.jsx
+++ src/components/Usuarios.jsx
@@ -7,7 +7,7 @@ import { ModalUserActualiza } from '../widgets/Modal/ModalUserActualiza'
 
 export const Usuarios = () => {
     return (
-        <section className="bg-gray-50 dark:bg-orange-600 py-3 sm:py-5">
+        <section className="bg-gradient-to-b from-pink-500 to-gray-900 py-3 sm:py-5">
         <div className="px-4 mx-auto max-w-screen-2xl lg:px-12">
             <div className="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
              

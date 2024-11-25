290cf865af85295b507107e4224307956310de6b
javiers78889 <javiers78889@gmail.com>
2024-11-25 17:23:25 -0500

    actualizado

--- src/widgets/Modal/ModalUser.jsx
+++ src/widgets/Modal/ModalUser.jsx
@@ -110,7 +110,7 @@ export const ModalUser = () => {
                                     <select id="countries" value={plan} onChange={onChangeModal} name='plan' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
                                         <option value="">Elige un Plan</option>
-                                        <option value="Entregado">Aereo</option>
+                                        <option value="aereo">Aereo</option>
                                         <option value="admin">Admin</option>
                                         <option value="pendiente">Maritimo</option>
 
